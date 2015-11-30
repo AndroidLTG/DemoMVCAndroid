539eb45a0cc85531ddebdc7b6b2da87a1040e262
AndroidLTG <androidltg94@gmail.com>
2015-11-30 15:00:14 +0700

    Fix code
    Short and clean code

--- app/src/main/java/com/example/mypc/demomvc/MainActivity.java
+++ app/src/main/java/com/example/mypc/demomvc/MainActivity.java
@@ -29,67 +29,49 @@ import CommonLib.EventString;
 import CommonLib.Model;
 import Controller.ControlThread;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ArrayAdapter<String> adapter;
     private ArrayList<String> arr = new ArrayList<>();
     private Timer timer;
     private TimerTask timerTask;
     private Handler handler = new Handler();
-    private RelativeLayout layout2,layout3,layoutMain;
+    private RelativeLayout layout2, layout3, layoutMain;
+    private Button btn, btnSwap1, btnSwap2, btnBack1, btnBack2;
+    private CheckBox chk;
+    private ListView lst;
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        getId();
+        eVent();
+    }
+
+    private void getId() {
+        layout2 = (RelativeLayout) findViewById(R.id.layout_2);
+        layout3 = (RelativeLayout) findViewById(R.id.layout_3);
+        layoutMain = (RelativeLayout) findViewById(R.id.layout_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+        btnSwap1 = (Button) findViewById(R.id.buttonSwap1);
+        btnSwap1.setOnClickListener(this);
+        btnSwap2 = (Button) findViewById(R.id.buttonSwap2);
+        btnSwap2.setOnClickListener(this);
+        btnBack1 = (Button) findViewById(R.id.btnBack1);
+        btnBack1.setOnClickListener(this);
+        btnBack2 = (Button) findViewById(R.id.btnBack2);
+        btnBack2.setOnClickListener(this);
+        chk = (CheckBox) findViewById(R.id.checkBox);
+        edit = (EditText) findViewById(R.id.editText);
+    }
+
+    private void eVent() {
         timer = new Timer();
         initializeTimerTask();
         timer.schedule(timerTask, 0, 100); //
-        Button btn = (Button) findViewById(R.id.button);
-        layout2  =(RelativeLayout) findViewById(R.id.layout_2);
-        layout3  =(RelativeLayout) findViewById(R.id.layout_3);
-        layoutMain = (RelativeLayout) findViewById(R.id.layout_main);
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                EditText edit = (EditText) findViewById(R.id.editText);
-                if (!edit.getText().toString().isEmpty()) {
-                    CommonLib.Model.inst().setText((edit.getText().toString()));
-                    EventPool.control().Enqueue(new EventBase(EventBase.Type.TEXT));
-                    edit.setText("");
-                }
-            }
-        });
-        Button btnSwap1 = (Button) findViewById(R.id.buttonSwap1);
-        btnSwap1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-              layout2.setVisibility(View.VISIBLE);
-                layoutMain.setVisibility(View.GONE);
-            }
-        });
-        Button btnSwap2 = (Button) findViewById(R.id.buttonSwap2);
-        btnSwap2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                layout3.setVisibility(View.VISIBLE);
-                layoutMain.setVisibility(View.GONE);
-            }
-        });
-        Button btnBack1  =(Button) findViewById(R.id.btnBack1);
-        btnBack1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                layout2.setVisibility(View.GONE);
-                layoutMain.setVisibility(View.VISIBLE);
-            }
-        });
-        Button btnBack2  =(Button) findViewById(R.id.btnBack2);
-        btnBack2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                layout3.setVisibility(View.GONE);
-                layoutMain.setVisibility(View.VISIBLE);
-            }
-        });
-        CheckBox chk = (CheckBox) findViewById(R.id.checkBox);
         chk.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 CheckBox chk = (CheckBox) findViewById(R.id.checkBox);
@@ -97,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 EventPool.control().Enqueue(new EventBase(EventBase.Type.CHECK));
             }
         });
-        ListView lst = (ListView) findViewById(R.id.listView);
+        lst = (ListView) findViewById(R.id.listView);
         adapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_activated_1, arr);
         lst.setAdapter(adapter);
@@ -115,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void initializeTimerTask() {
-
         timerTask = new TimerTask() {
             public void run() {
                 handler.post(new Runnable() {
@@ -141,5 +122,34 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         };
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                if (!edit.getText().toString().isEmpty()) {
+                    CommonLib.Model.inst().setText((edit.getText().toString()));
+                    EventPool.control().Enqueue(new EventBase(EventBase.Type.TEXT));
+                    edit.setText("");
+                }
+                break;
+            case R.id.buttonSwap1:
+                layout2.setVisibility(View.VISIBLE);
+                layoutMain.setVisibility(View.GONE);
+                break;
+            case R.id.buttonSwap2:
+                layout3.setVisibility(View.VISIBLE);
+                layoutMain.setVisibility(View.GONE);
+                break;
+            case R.id.btnBack1:
+                layout2.setVisibility(View.GONE);
+                break;
+            case R.id.btnBack2:
+                layout3.setVisibility(View.GONE);
+                layoutMain.setVisibility(View.VISIBLE);
+                break;
+
+        }
     }
 }
